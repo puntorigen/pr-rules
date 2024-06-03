d286e8d5a998c1c41efc0ae516c600e894e9661f
Pablo Schaffner <pablo@puntorigen.com>
2024-06-02 22:57:13 -0400

    - added more test methods

--- testfile.py
+++ testfile.py
@@ -2,3 +2,11 @@ print("hello there")
 this_is_valid = 24
 #sql = "SELECT * FROM table WHERE column = 'value'"
 
+def hello():
+    print("hello")
+
+def hn(name:str):
+    print(f"hello {name}")
+
+def add(a:int,b:int):
+    return a+b
