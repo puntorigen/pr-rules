2fc0b5b66120b50e0e27fc6608aec49ba05df7bc
Pablo Schaffner <pablo@puntorigen.com>
2024-06-03 21:48:38 -0400

    - added test for ollama

---
+++ tests/test1.py
@@ -0,0 +1,14 @@
+def hello_world():
+    print("Hello World!")
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+    def lon(self):
+        # length of name
+        return len(self.name)
