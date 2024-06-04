3cbfccc1509a8fb9e2e62b7a4e91c846acafce1d
Pablo Schaffner <pablo@puntorigen.com>
2024-06-04 12:32:28 -0400

    - swapped order of ollama cmds on install_ollama sh

--- install_ollama.sh
+++ install_ollama.sh
@@ -2,8 +2,9 @@
 echo "OpenAI API key not provided. Installing Ollama..."
 curl -fsSL https://ollama.com/install.sh | sh
 sleep 10
-echo "Downloading Ollama model... phi3:3.8b-mini-128k-instruct-q8_0"
-ollama pull phi3:3.8b-mini-128k-instruct-q8_0
-sleep 5
 echo "Starting Ollama server ..."
 ollama serve &
+sleep 10
+echo "Downloading Ollama model... phi3:3.8b-mini-128k-instruct-q8_0"
+ollama pull "phi3:3.8b-mini-128k-instruct-q8_0"
+sleep 10
